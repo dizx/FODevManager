c9657b20ad11c67548e243b2db355647c61b8d42
Morten Aasheim <mortenaa@gmail.com>
2025-03-16 17:06:59 +0700

    Added commandparser and tests

---
+++ FODevManager.Tests/CommandParserTests.cs
@@ -0,0 +1,65 @@
+﻿using FODevManager.Utils;
+
+namespace FODevManager.Tests
+{ 
+    [TestFixture]
+    public class CommandParserTests
+    {
+        [Test]
+        public void Should_Parse_Profile_Create_Command()
+        {   
+            string[] args = { "-profile", "YM", "create" };
+            var parser = new CommandParser(args);
+
+            Assert.That(parser.IsValid, Is.True);
+            Assert.That(parser.ProfileName, Is.EqualTo("YM"));
+            Assert.That(parser.ModelName, Is.Null);
+            Assert.That(parser.Command, Is.EqualTo("create"));
+        }
+
+        [Test]
+        public void Should_Parse_Model_Add_Command()
+        {
+            string[] args = { "-profile", "YM", "-model", "PtsTools", "add", "C:\\Path\\to\\project.rnrproj" };
+            var parser = new CommandParser(args);
+
+            Assert.That(parser.IsValid, Is.True);
+            Assert.That(parser.ProfileName, Is.EqualTo("YM"));
+            Assert.That(parser.ModelName, Is.EqualTo("PtsTools"));
+            Assert.That(parser.Command, Is.EqualTo("add"));
+            Assert.That(parser.FilePath, Is.EqualTo("C:\\Path\\to\\project.rnrproj"));
+        }
+
+        [Test]
+        public void Should_Fail_When_Profile_Missing()
+        {
+            string[] args = { "create" };
+            var parser = new CommandParser(args);
+
+            Assert.That(parser.IsValid, Is.False);
+        }
+
+        [Test]
+        public void Should_Fail_When_Command_Missing()
+        {
+            string[] args = { "-profile", "YM" };
+            var parser = new CommandParser(args);
+
+            Assert.That(parser.IsValid, Is.False);
+        }
+
+        [TestCase(new string[] { "-profile", "YM", "-model", "PtsTools", "deploy" }, "YM", "PtsTools", "deploy", null)]
+        [TestCase(new string[] { "-profile", "YM", "-model", "PtsTools", "add", "C:\\Path\\to\\project.rnrproj" }, "YM", "PtsTools", "add", "C:\\Path\\to\\project.rnrproj")]
+        [TestCase(new string[] { "-profile", "DevEnv", "check" }, "DevEnv", null, "check", null)]
+        public void Should_Parse_Valid_Commands(string[] args, string expectedProfile, string expectedModel, string expectedCommand, string expectedFilePath)
+        {
+            var parser = new CommandParser(args);
+
+            Assert.That(parser.IsValid, Is.True);
+            Assert.That(parser.ProfileName, Is.EqualTo(expectedProfile));
+            Assert.That(parser.ModelName, Is.EqualTo(expectedModel));
+            Assert.That(parser.Command, Is.EqualTo(expectedCommand));
+            Assert.That(parser.FilePath, Is.EqualTo(expectedFilePath));
+        }
+    }
+}
---
+++ FODevManager/Utils/CommandParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FODevManager.Utils
+{
+    public class CommandParser
+    {
+        public string ProfileName { get; private set; }
+        public string ModelName { get; private set; }
+        public string Command { get; private set; }
+        public string FilePath { get; private set; }
+        public bool IsValid { get; private set; }
+
+        public CommandParser(string[] args)
+        {
+            ParseArguments(args);
+        }
+
+        private void ParseArguments(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error: Insufficient arguments provided.");
+                IsValid = false;
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-profile":
+                        if (i + 1 < args.Length) ProfileName = args[++i];
+                        break;
+
+                    case "-model":
+                        if (i + 1 < args.Length) ModelName = args[++i];
+                        break;
+
+                    default:
+                        if (Command == null)
+                            Command = args[i];
+                        else
+                            FilePath = args[i];
+                        break;
+                }
+            }
+
+            // Ensure required arguments are present
+            IsValid = !string.IsNullOrEmpty(ProfileName) && !string.IsNullOrEmpty(Command);
+        }
+    }
+}
