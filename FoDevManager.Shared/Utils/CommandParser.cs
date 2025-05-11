fe0a08d99d50971257de8667dcf7d3957cc72f83
Morten Aasheim <mortenaa@gmail.com>
2025-05-11 15:42:29 +0700

    Fixed error in command parser

--- FoDevManager.Shared/Utils/CommandParser.cs
+++ FoDevManager.Shared/Utils/CommandParser.cs
@@ -161,7 +161,7 @@ namespace FODevManager.Utils
             var knownCommands = new[] {
                 "create", "delete", "check", "list",
                 "add", "remove", "deploy", "undeploy",
-                "git-check", "git-open", "git-status", "import"
+                "git-check", "git-open", "git-status", "import",
                 "switch", "db-set", "db-apply", "peri"
         };
 
