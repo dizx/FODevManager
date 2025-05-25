9e76cd003d83af4da57a576b5d72495d2e948d3c
Morten Aasheim <mortenaa@gmail.com>
2025-05-25 15:52:27 +0700

    Added Create Model function
    Create a new FO Model from scratch, and add project to solution

--- FoDevManager.Shared/Utils/AppConfig.cs
+++ FoDevManager.Shared/Utils/AppConfig.cs
@@ -13,11 +13,18 @@ namespace FODevManager.Utils
         public string DeploymentBasePath { get; set; }
         public string DefaultSourceDirectory { get; set; }
 
+        public int ModelIdBegin { get; set; }
+
+        public int ModelIdEnd { get; set; }
+
         public AppConfig(IConfiguration configuration)
         {
             ProfileStoragePath = Environment.ExpandEnvironmentVariables(configuration["ProfileStoragePath"]);
             DeploymentBasePath = Environment.ExpandEnvironmentVariables(configuration["DeploymentBasePath"]);
             DefaultSourceDirectory = Environment.ExpandEnvironmentVariables(configuration["DefaultSourceDirectory"]);
+            ModelIdBegin = int.Parse(Environment.ExpandEnvironmentVariables(configuration["ModelIdBegin"] ?? "896001001"));
+            ModelIdEnd = int.Parse(Environment.ExpandEnvironmentVariables(configuration["ModelIdEnd"] ?? "896009999"));
+
         }
     }
 }
