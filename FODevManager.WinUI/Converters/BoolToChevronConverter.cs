20e17876f0cc78625a0a8f012fef969aedc9dd59
Morten Aasheim <maa@peritus.no>
2025-10-28 22:57:32 +0700

    New converters

---
+++ FODevManager.WinUI/Converters/BoolToChevronConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FODevManager.WinUI.Converters
+{
+    public sealed class BoolToChevronConverter : IValueConverter
+    {
+        // Collapsed: ChevronRight (E76C). Expanded: ChevronDown (E70D).
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => (value as bool? == true) ? "\uE70D" : "\uE76C";
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => false;
+    }
+
+}
---
+++ FODevManager.WinUI/Converters/ModelTypeToBrushConverter.cs
@@ -0,0 +1,19 @@
+﻿using FODevManager.Models;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace FODevManager.WinUI.Converters
+{
+    public sealed class ModelTypeToBrushConverter : IValueConverter
+    {
+        public SolidColorBrush SourceBrush { get; set; } = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255));
+        public SolidColorBrush CompiledBrush { get; set; } = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 86, 156, 214));
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => value is ModelType mt && mt == ModelType.Compiled ? CompiledBrush : SourceBrush;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
